458a74457c69718ada3365d156df21edb85e707a
David Malmstrøm <david@1420.dk>
2025-03-15 22:37:47 +0100

    Added an install script for new machines

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Variables
+REPO_URL="https://github.com/Diamnond/NixOS.git"
+HOSTNAME="nixos02"
+
+# Clone the repository to home the directory
+cd ~
+git clone $REPO_URL
+
+# Generate a new hardware configuration
+nixos-generate-config
+
+# Copy the hardware configuration to the repository
+cp /etc/nixos/hardware-configuration.nix ~/NixOS/hosts/$HOSTNAME/
+
+# rebuild the system
+nixos-rebuild switch --flake ~/NixOS/#$HOSTNAME
+
+# Reboot the system
+reboot
