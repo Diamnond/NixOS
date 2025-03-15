f077c7e95b50e240b19ec6195088d7bb9454e57c
David Malmstrøm <david@1420.dk>
2025-03-15 23:20:48 +0100

    Fixed Install script

--- install.sh
+++ install.sh
@@ -1,21 +1,20 @@
-#!/bin/bash
-
 # Variables
 REPO_URL="https://github.com/Diamnond/NixOS.git"
 HOSTNAME=$1
+USER=$SUDO_USER
+
+# Install git
+nix-env --install git
 
 # Clone the repository to home the directory
-cd ~
+cd /home/$USER/
 git clone $REPO_URL
 
 # Generate a new hardware configuration
-nixos-generate-config
+sudo nixos-generate-config
 
 # Copy the hardware configuration to the repository
-cp /etc/nixos/hardware-configuration.nix ~/NixOS/hosts/$HOSTNAME/
+cp /etc/nixos/hardware-configuration.nix /home/$USER/NixOS/hosts/$HOSTNAME/hardware-configuration.nix
 
 # rebuild the system
-nixos-rebuild switch --flake ~/NixOS/#$HOSTNAME
-
-# Reboot the system
-reboot
+sudo nixos-rebuild switch --flake /home/$USER/NixOS/#$HOSTNAME
