dcae9ca5b9f670692f76bd498cfbbf5389743c47
David Malmstrøm <david@1420.dk>
2025-03-16 11:06:18 +0100

    Added descriptions to packages in default apps + VLC

--- modules/nix/default-apps.nix
+++ modules/nix/default-apps.nix
@@ -2,16 +2,18 @@
 
 {  
   environment.systemPackages = with pkgs; [
-    wget
-    brave
-    thunderbird
-    vscode
-    git
-    curl
-    htop
-    fastfetch
-    meld
-    bottles
+    wget # downloads files
+    brave # browser
+    thunderbird # mail client
+    vscode # code editor
+    git # version control
+    curl # downloads files
+    htop # system monitor
+    fastfetch # system info
+    meld # file comparison
+    bottles # wine manager
+    vlc # media player
+    
   ];
 }
 
